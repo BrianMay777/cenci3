3674557388ff6b94adcdb74a93a5142fdbe0c163
Bram Swenson <bram@craniumisajar.com>
2011-10-10 23:41:27 +0000

    [Initial] rails app layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cenci3::Application.config.secret_token = 'd5883dac62a59ac305ffff492dd32c8e127d69c85c7e4a590dc7b1c1fcbb54a54399503644a7dbb7cb83de848b0deefcbca196d756701049dfe860bb1d4a5692'
